175d32559cd428333293753e60ef7081581d98ad
Enrique Muñoz <enriquemunozdev@gmail.com>
2023-12-25 01:21:48 -0600

    kept going

--- src/Autocomplete.tsx
+++ src/Autocomplete.tsx
@@ -1,9 +1,17 @@
 import { ChangeEvent, useState, useEffect } from "react";
+import { books_data, Data } from "./data";
 
 interface AutocompleteArgs {
   id_name: string;
   placeholder: string;
   label: string;
+}
+
+const fetchData = async (): Promise<Data[]> => {
+  await new Promise(resolve => setTimeout(resolve, 1500));
+
+  return books_data;
+
 }
 
 const Autocomplete = (args: AutocompleteArgs) => {
@@ -19,6 +27,8 @@ const Autocomplete = (args: AutocompleteArgs) => {
     } else {
       console.log(text);
     }
+
+    fetchData();
   }, [text]);
 
   return (
@@ -42,14 +52,22 @@ const Autocomplete = (args: AutocompleteArgs) => {
       </div>
 
       <div>
-        <div className="w-full p-2 bg-white border border-t-0 shadow-md border-gray-200 rounded-sm">
-            Holi
-        </div>
-        <div className="w-full p-2 bg-white border border-t-0 shadow-md border-gray-200 rounded-sm">
-            Holi
-        </div>
+        {
+          books_data.map((book) => (<AutocompleteOption id={book.id} str={book.str} />))
+        }   
       </div>
     </div>
+  );
+};
+
+const AutocompleteOption = (args: Data) => {
+  return (
+      <div 
+        className="w-full p-2 bg-white border border-t-0 shadow-md border-gray-200 rounded-sm"
+        data-value={args.id}
+      >
+        {args.str}
+      </div>
   );
 };
 
